fa96f6639dffa98ba0c01cecc512c0fe6ffc04de
mariamiah <mariah.kakande@gmail.com>
2020-05-08 08:55:25 +0300

    add comments to code

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 from src.app import app
 
+# run the application
 if __name__ == '__main__':
     app.run()
--- src/app.py
+++ src/app.py
@@ -1,7 +1,9 @@
 from flask import Flask
 
+# initialize the flask application
 app = Flask(__name__)
 
+# setup a test route
 @app.route('/')
 def hello():
     return 'hello world!'
