000d73daddd629448876ee19e82e2a10612e0be8
lubwamabenja <lubwama73@gmail.com>
2020-05-19 05:47:19 -0700

    admin should be able to add a course #19

--- src/models/validator.py
+++ src/models/validator.py
@@ -31,3 +31,57 @@ class ValidateUser:
             return False
         else:
             return True
+
+
+class ValidateCourse:
+    ''' 
+    Class validates the courses added to the database 
+
+    '''
+
+    def __init__(self, categoryId, instructorId, duration, courseName):
+        self.categoryId = categoryId
+        self.instructorId = instructorId
+        self.duration = duration
+        self.courseName = courseName
+
+    def validate_categoryId(self):
+        ''' 
+        function validates the category id
+
+        '''
+        if not self.categoryId and not isinstance(
+                self.categoryId, int):
+            return False
+        else:
+            return True
+
+    def validate_coursename(self):
+        ''' function validates the course name '''
+
+        if not self.courseName or self.courseName.isspace() or not isinstance(
+                self.courseName, str):
+            return False
+        else:
+            return True
+
+    def validate_instructorId(self):
+        ''' 
+        function validates the instructor id
+        '''
+        if not self.instructorId and not isinstance(
+                self.instructorId, int):
+            return False
+        else:
+            return True
+
+    def validate_Duration(self):
+        ''' 
+        function validates the course Duration
+
+        '''
+        if not self.duration and not isinstance(
+                self.duration, int):
+            return False
+        else:
+            return True
