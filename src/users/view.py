90d3c93bc3a564bdfca96b34fb0812f2a18ca4c6
skizza8 <skizza8@gmail.com>
2020-05-20 21:14:05 +0300

    view method for user signup API route

--- src/users/view.py
+++ src/users/view.py
@@ -1,6 +1,6 @@
 from flask import jsonify, Blueprint, request
 from .controller import UserController
-from src.validators.user_validator import ValidateUser
+from src.validators.user_validator import ValidateUser, ValidateUserRegistration
 
 
 user = Blueprint('user', __name__)
@@ -10,7 +10,29 @@ user_controller = UserController()
 @user.route('/api/v1/auth/signup', methods=['POST'])
 def register_user():
     """Registers a User."""
-    return jsonify({"message": "user registered successfully"})
+    data = request.get_json()
+    username = data['username']
+    password = data['password']
+    email = data['email']
+    firstname = data['firstname']
+    lastname = data['lastname']
+    role = data['role']
+    confirm_password = data['confirm_password']
+
+    '''
+    First we validate the data submitted by the user for registration
+    '''
+    validate = ValidateUserRegistration(username,password,confirm_password,email,firstname,lastname,role)
+    
+    validation_message, registration_data_is_valid = validate.validate_registration_details() 
+    
+   
+    if registration_data_is_valid:
+        return user_controller.create_user(username,password,email,firstname,lastname,role)
+    else:
+        return jsonify({"message":"{}".format(validation_message)}), 400
+
+    
 
 
 @user.route('/api/v1/auth/login', methods=['POST'])
@@ -21,6 +43,7 @@ def login():
     username = data['username']
     password = data['password']
     validate = ValidateUser(username, password)
+   
     if validate.validate_username() and \
        validate.validate_password():
         return user_controller.login_user(username, password)
