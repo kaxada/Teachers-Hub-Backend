389b857ce56cfb94929ab98aad50c097b4bb0f39
lubwamabenja <lubwama73@gmail.com>
2020-05-25 04:02:52 -0700

    An admin should be able to update a course

--- src/courses/controller.py
+++ src/courses/controller.py
@@ -29,7 +29,15 @@ class CourseController:
     def query_course(self, course_id):
         ''' selects a course from database '''
         sql = """ SELECT * FROM courses  WHERE id ='{}' """
-        sql_command = sql.format(course_id)     
+        sql_command = sql.format(course_id)
         self.cur.execute(sql_command)
         row = self.cur.fetchone()
         return row
+
+    def update_course(self, data, course_id):
+        """Updates a course."""
+        sql = """UPDATE courses SET course_name='{}', course_duration='{}'\
+        WHERE course_id='{}'"""
+        sql_command = sql.format(data['course_name'],
+                                 data['course_duration'], course_id)
+        self.cur.execute(sql_command)
