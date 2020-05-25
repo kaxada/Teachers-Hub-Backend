8fd535576d5a52e11c4530fe4f514104bb58d681
lubwamabenja <lubwama73@gmail.com>
2020-05-25 14:17:35 -0700

    Revert "merge conflicts"
    This reverts commit d7918720ce20bd12165b37d61c29a57edd96529d.

--- src/courses/controller.py
+++ src/courses/controller.py
@@ -22,13 +22,88 @@ class CourseController:
 
     def delete_course(self, course_id):
         ''' Deletes a course '''
-        sql = """ DELETE FROM courses WHERE id ='{}'""".format(course_id)        
-        self.cur.execute(sql)
+<<<<<<< HEAD
+<<<<<<< HEAD
+        sql = """ DELETE FROM courses WHERE courseID ='{}'"""
+        sql_command = sql.format(course_id)
+        self.cur.execute(sql_command)
 
     def query_course(self, course_id):
         ''' selects a course from database '''
-        sql = """ SELECT * FROM courses  WHERE id ='{}' """.format(course_id)       
-        self.cur.execute(sql)
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+        sql = """ SELECT * FROM courses  WHERE CourseID ='{}' """
+        sql_command = sql.format(course_id)
+        self.cur.execute(sql_command)
         row = self.cur.fetchone()
         return row
+
+    def update_course(self, data, course_id):
+        """Updates a course."""
+        sql = """UPDATE courses SET course_name='{}', course_duration='{}'\
+        WHERE CourseID='{}'"""
+        sql_command = sql.format(data['course_name'],
+                                 data['course_duration'], course_id)
+        self.cur.execute(sql_command)
+>>>>>>> ca2ce73b24da4cd08e4dcfd792d7b2ddf9775c55
+        sql = """ SELECT * FROM courses  WHERE courseID ='{}' """
+        sql_command = sql.format(course_id)
+        self.cur.execute(sql_command)
+        row = self.cur.fetchone()
+        if row:
+            return row
+<<<<<<< HEAD
+=======
+        sql = """ DELETE FROM courses WHERE id ='{}'""".format(course_id)        
+        self.cur.execute(sql)
+=======
+        sql = """ DELETE FROM courses WHERE id ='{}'"""
+        sql_command = sql.format(course_id)
+        self.cur.execute(sql_command)
+>>>>>>> remove injection
+
+    def query_course(self, course_id):
+        ''' selects a course from database '''
+        sql = """ SELECT * FROM courses  WHERE id ='{}' """
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+        sql = """ SELECT * FROM courses  WHERE CourseID ='{}' """
+>>>>>>> change course_id to CourseID
+        sql_command = sql.format(course_id)
+=======
+        sql_command = sql.format(course_id)     
+>>>>>>> removing trailing white space
+        self.cur.execute(sql_command)
+        row = self.cur.fetchone()
+<<<<<<< HEAD
+<<<<<<< HEAD
+        return row
     
+>>>>>>> An administrator should be able to delete a course
+=======
+        return row
+>>>>>>> remove trailing white space
+=======
+=======
+        sql_command = sql.format(course_id)
+        self.cur.execute(sql_command)
+        row = self.cur.fetchone()
+>>>>>>> An admin should be able to update a course
+        return row
+
+    def update_course(self, data, course_id):
+        """Updates a course."""
+        sql = """UPDATE courses SET course_name='{}', course_duration='{}'\
+        WHERE CourseID='{}'"""
+        sql_command = sql.format(data['course_name'],
+                                 data['course_duration'], course_id)
+<<<<<<< HEAD
+        self.cur.execute(sql_command)
+>>>>>>> An admin should be able to update a course
+=======
+        self.cur.execute(sql_command)
+>>>>>>> An admin should be able to update a course
+=======
+>>>>>>> ca2ce73b24da4cd08e4dcfd792d7b2ddf9775c55
--- src/courses/view.py
+++ src/courses/view.py
@@ -34,10 +34,16 @@ def delete_course(course_id):
     """
     Function enables admin to delete a course from the database.
 <<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
 
 =======
     
 >>>>>>> An administrator should be able to delete a course
+=======
+>>>>>>> removing trailing white space
+=======
+>>>>>>> removing trailing white space
     """
     try:
         course_id = int(course_id)
@@ -54,3 +60,112 @@ def delete_course(course_id):
         return jsonify({
             'message': 'The course id should be an integer!'
         }), 400
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+=======
+
+>>>>>>> An administrator should be able to delete a course
+=======
+>>>>>>> remove trailing white space
+=======
+=======
+>>>>>>> An administrator should be able to view a single course
+=======
+>>>>>>> ca2ce73b24da4cd08e4dcfd792d7b2ddf9775c55
+
+
+@course.route('/api/v1/courses/<course_id>', methods=['GET'])
+def view_course(course_id):
+    """
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+    Function enables user to view a course from the database.
+=======
+    Function enables admin to view a course from the database.
+    
+>>>>>>> An administrator should be able to view a single course
+=======
+    Function enables user to view a course from the database.
+>>>>>>> An admin should be able to update a course
+=======
+    Function enables user to view a course from the database.
+>>>>>>> ca2ce73b24da4cd08e4dcfd792d7b2ddf9775c55
+    """
+    try:
+        course_id = int(course_id)
+        if not course_controller.query_course(course_id):
+            return jsonify({
+                'message': 'Course does not exist in database'
+            }), 400
+        course = course_controller.query_course(course_id)
+        return jsonify({
+            'course': {
+                '_id': course[0],
+                'course_name': course[1],
+                'course_duration': course[2]
+            },
+            'message': 'course fetched!'
+        }), 200
+    except ValueError:
+        return jsonify({
+            'message': 'The course id should be an integer!'
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+        }), 400
+>>>>>>> An administrator should be able to view a single course
+=======
+        }), 400
+>>>>>>> removing trailing white space
+=======
+=======
+>>>>>>> An admin should be able to update a course
+=======
+>>>>>>> ca2ce73b24da4cd08e4dcfd792d7b2ddf9775c55
+        }), 400
+
+
+@course.route('/api/v1/courses/<course_id>', methods=['PUT'])
+def update_course(course_id):
+    """
+    Function enables user to modify a course from the database.
+    """
+    data = request.get_json()
+
+    if data:
+        validate_course = ValidateCourse(data)
+        try:
+            if validate_course.validate_course_name() and \
+               validate_course.validate_course_duration():
+                course_controller.update_course(data, course_id)
+                return jsonify({"message": "course updated successfully"}), 200
+            elif not validate_course.validate_course_name():
+                return jsonify({"message": "enter valid course name"}), 400
+            elif not validate_course.validate_course_duration():
+                return jsonify({"message": "enter valid course duration"}), 400
+        except ValueError:
+            return jsonify({"message": "course id should be an integer"}), 400
+    else:
+<<<<<<< HEAD
+<<<<<<< HEAD
+        return jsonify({"message": "course details not provided"}), 400
+>>>>>>> An admin should be able to update a course
+=======
+        }), 400
+>>>>>>> An administrator should be able to view a single course
+=======
+        }), 400
+>>>>>>> removing trailing white space
+=======
+        return jsonify({"message": "course details not provided"}), 400
+>>>>>>> An admin should be able to update a course
+=======
+        return jsonify({"message": "course details not provided"}), 400
+>>>>>>> ca2ce73b24da4cd08e4dcfd792d7b2ddf9775c55
