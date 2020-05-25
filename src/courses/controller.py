b34cf44170b323ea826f8667db3ebeb05887e6d2
lubwamabenja <lubwama73@gmail.com>
2020-05-25 02:16:31 -0700

    An administrator should be able to delete a course

--- src/courses/controller.py
+++ src/courses/controller.py
@@ -19,3 +19,16 @@ class CourseController:
         sql_command = sql.format(data['course_name'],
                                  data['course_duration'])
         self.cur.execute(sql_command)
+
+    def delete_course(self, course_id):
+        ''' Deletes a course '''
+        sql = """ DELETE FROM courses WHERE id ='{}'""".format(course_id)        
+        self.cur.execute(sql)
+
+    def query_course(self, course_id):
+        ''' selects a course from database '''
+        sql = """ SELECT * FROM courses  WHERE id ='{}' """.format(course_id)       
+        self.cur.execute(sql)
+        row = self.cur.fetchone()
+        return row
+    
