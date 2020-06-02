b73cc8fad44a9a89a30977b461c7ee05fe7a9a12
skizza8 <skizza8@gmail.com>
2020-06-02 16:53:01 +0300

    Added route for user to query profile details

--- src/users/view.py
+++ src/users/view.py
@@ -1,6 +1,7 @@
 from flask import jsonify, Blueprint, request
 from .controller import UserController
 from src.validators.user_validator import ValidateUser
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 user = Blueprint('user', __name__)
 user_controller = UserController()
@@ -29,3 +30,11 @@ def login():
         return jsonify({
             "message": validate.validate_login()
         }), 400
+
+
+@user.route('/api/v1/auth/profile', methods=['GET'])
+@jwt_required
+def get_user_profile():
+    current_user = get_jwt_identity()
+    profile_details = user_controller.get_user_profile_details(current_user)
+    return jsonify(username=profile_details[2], email=profile_details[1], role=profile_details[4]), 200
