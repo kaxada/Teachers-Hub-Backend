16d10a279c9b5bcb01b74b70af78f1d8b4d112b2
mariamiah <mariah.kakande@gmail.com>
2020-06-06 14:15:27 +0300

    add ability to add module content to a specific module

--- src/validators/module_validator.py
+++ src/validators/module_validator.py
@@ -5,7 +5,7 @@ class ModuleValidator:
 
     def validate_module_fields(self):
         """Validates module details"""
-        module_fields = ["module_title","module_description","module_content"]
+        module_fields = ["module_title","module_description"]
         for field in module_fields:
             if not self.data[field]:
                 return field + " cannot be blank"
@@ -14,9 +14,27 @@ class ModuleValidator:
             if not isinstance(self.data[field], str):
                 return "Enter string value at {}".format(field)
 
+    def validate_module_content(self):
+        """Validates module content"""
+        module_content_fields=["module_content", "module_content_title"]
+        for field in module_content_fields:
+            if not self.data[field]:
+                return field + "cannot be blank"
+            if field not in self.data.keys():
+                return field + " is missing"
+            if not isinstance(self.data[field], str):
+                return "Enter string value at {}".format(field)
+
+
     def is_valid(self):
         """combines all field validation"""
         if isinstance(self.validate_module_fields(), str):
             return self.validate_module_fields()
+        else:
+            return "valid"
+
+    def is_module_valid(self):
+        if isinstance(self.validate_module_content(), str):
+            return self.validate_module_content()
         else:
             return "valid"
