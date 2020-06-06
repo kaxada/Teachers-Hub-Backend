8895b8d06f0ea9c257cbf3993f4ea5f0d9502bcf
mariamiah <mariah.kakande@gmail.com>
2020-06-06 14:15:27 +0300

    add ability to add module content to a specific module

--- src/articles/view.py
+++ src/articles/view.py
@@ -1,34 +1,44 @@
-from flask import jsonify, Blueprint
+from flask import jsonify, Blueprint, request
 from .controller import ArticleController
+from ..validators.article_validator import ValidateArticle
 
 article = Blueprint('article', __name__)
 article_controller = ArticleController()
 
 
-@article.route('/api/v1/articles/<article_id>', methods=['GET'])
-def view_article(article_id):
+@article.route('/api/v1/articles/<article_id>', methods=['PUT'])
+def update_article(article_id):
+    """posts a new article"""
+    data = request.get_json()
+
+    if data:
+        validate_article = ValidateArticle(data)
+        if validate_article.validate_article_title() and\
+            validate_article.validate_author_name() and\
+            validate_article.validate_article_body():
+            article_controller.update_article(data, article_id)
+            return jsonify({"message": "article updated successfully"}), 200
+        elif not validate_article.validate_article_title():
+            return jsonify({"message": "enter valid article title"}), 400
+        elif not validate_article.validate_author_name():
+            return jsonify({"message": "enter valid author name"}), 400
+        elif not validate_article.validate_article_body():
+            return jsonify({"message": "enter valid article body "}), 400
+    else:
+        return jsonify({"message": "article details not provided"}), 400
+
+
+@article.route('/api/v1/articles', methods=['GET'])
+def view_all_articles():
     """
-    Function enables user to view an article from the database.
+    Function enables user to view all the available articles from the database.
     """
-    try:
-        article_id = int(article_id)
-        if not article_controller.query_article(article_id):
-            return jsonify({
-                'message': 'article does not exist in database'
-            }), 400
-        article = article_controller.query_article(article_id)
+    if not article_controller.query_all_articles():
         return jsonify({
-            'article': {
-                '_id': article[0],
-                'article_title': article[1],
-                'author_name': article[2],
-                'created_at': article[3],
-                'updates_at': article[4],
-                'article_body': article[5],
-            },
-            'message': 'article fetched!'
-        }), 200
-    except ValueError:
-        return jsonify({
-            'message': 'The article id should be an integer!'
+            'message': 'No available articles in the database'
         }), 400
+    articles = article_controller.query_all_articles()
+    return jsonify({
+        'articles': articles,
+        'message': 'articles fetched!'
+    }), 200
