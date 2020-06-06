810fd90cfcf76aa54310d8b019e9c4da045cf6f6
mariamiah <mariah.kakande@gmail.com>
2020-06-06 14:15:27 +0300

    add ability to add module content to a specific module

--- src/articles/view.py
+++ src/articles/view.py
@@ -52,3 +52,32 @@ def delete_article(article_id):
         return jsonify({
             'message': 'The article id should be an integer!'
         }), 400
+
+
+@article.route('/api/v1/articles/<article_id>', methods=['GET'])
+def view_article(article_id):
+    """
+    Function enables user to view an article from the database.
+    """
+    try:
+        article_id = int(article_id)
+        if not article_controller.query_article(article_id):
+            return jsonify({
+                'message': 'article does not exist in database'
+            }), 400
+        article = article_controller.query_article(article_id)
+        return jsonify({
+            'article': {
+                '_id': article[0],
+                'article_title': article[1],
+                'author_name': article[2],
+                'created_at': article[3],
+                'updates_at': article[4],
+                'article_body': article[5],
+            },
+            'message': 'article fetched!'
+        }), 200
+    except ValueError:
+        return jsonify({
+            'message': 'The article id should be an integer!'
+        }), 400
